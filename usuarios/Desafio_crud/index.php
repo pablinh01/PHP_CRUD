b28509e6013a0b42cbfc310ce4b22b85b5bde087
pablinh01 <pablohenriquetorresgama@gmail.com>
2024-07-05 11:24:35 -0300

    Add files via upload

---
+++ usuarios/Desafio_crud/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>cadastrar</h1>
+    <!-- Caixa para cadastrar e listar  -->
+    <form action="processo.php" method="post">
+        <div id="nome">
+            <label for="">Nome:</label>
+            <input type="text" name="nome" placeholder="Coloque seu email">
+        </div id="email">
+        <div id="email">
+            <label for="">email</label>
+            <input type="email" name="email" placeholder="Coloque seu email">
+        </div>
+        <div id="botao">
+            <input type="submit" value="cadastro">
+        </div>
+    </form>
+</body>
+
+</html>
